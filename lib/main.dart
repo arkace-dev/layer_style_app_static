e622b4a72be844456ce11e821ea7085d132b1ee2
Arsh <arsh.js.dev@gmail.com>
2020-04-13 22:14:14 +0530

    code complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,446 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+    statusBarIconBrightness: Brightness.dark,
+  ));
+
+  runApp(MyApp());
+}
+
+const Color pinkBox = Color(0xFFE0719F);
+const Color purpleBox = Color(0xFF8843AE);
+const Color ultraVioletBox = Color(0xFF1B062A);
+const Color greyBox = Color(0xFF8C92B2);
+
+Widget avatarBuilder(String imgName) {
+  return Container(
+    width: 30,
+    height: 30,
+    child: CircleAvatar(
+      backgroundImage: AssetImage('assets/images/$imgName'),
+    ),
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(15),
+      border: Border.all(
+        color: Colors.white,
+        width: 2,
+      ),
+    ),
+  );
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
+        fontFamily: 'Raleway',
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+    return SafeArea(
+      top: false,
+      child: Scaffold(
+        backgroundColor: greyBox,
+        body: Column(
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
+            TopConatiner(),
+            PinkBox(),
+            PurpleBox(),
+            UltraVioletBox(),
           ],
         ),
+        floatingActionButton: FloatingActionButton(
+          child: const Icon(
+            Icons.add,
+            color: ultraVioletBox,
+          ),
+          onPressed: () {},
+          backgroundColor: Colors.white,
+        ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class TopConatiner extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 160,
+      color: pinkBox,
+      child: Material(
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(60),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(
+            left: 16.0,
+            right: 16.0,
+            top: 24.0,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: <Widget>[
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  CircleAvatar(
+                    radius: 30,
+                    backgroundImage: AssetImage('assets/images/4.jpg'),
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  const Text('You'),
+                ],
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    width: 60,
+                    height: 60,
+                    child: const Icon(
+                      Icons.trending_up,
+                      size: 30,
+                    ),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.pink,
+                      ),
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  const Text(
+                    'Trending',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    width: 60,
+                    height: 60,
+                    child: const Icon(
+                      Icons.favorite_border,
+                      size: 30,
+                      color: Colors.grey,
+                    ),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.grey[300],
+                      ),
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  const Text(
+                    'Health',
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class PinkBox extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 190,
+      decoration: BoxDecoration(
+        border: Border(
+          top: BorderSide(
+            color: pinkBox,
+            width: 0,
+          ),
+        ),
+        color: purpleBox,
+      ),
+      child: Material(
+        color: pinkBox,
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(60),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            vertical: 24.0,
+            horizontal: 32.0,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              const SizedBox(
+                height: 4,
+              ),
+              const Text(
+                'TODAY 5:30 P.M.',
+                style: TextStyle(
+                  fontWeight: FontWeight.w300,
+                  color: Colors.white,
+                ),
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              const Text(
+                'Yoga and Meditation Class for Beginners',
+                style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  fontSize: 24,
+                  color: Colors.white,
+                ),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      width: 50,
+                      child: Stack(
+                        children: <Widget>[
+                          Positioned(
+                            child: avatarBuilder('1.jpg'),
+                            left: 15,
+                          ),
+                          avatarBuilder('2.jpg'),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 16,
+                    ),
+                    const Text(
+                      'Frank Martin, Bob and 10 others',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w300,
+                        fontStyle: FontStyle.italic,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class PurpleBox extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 190,
+      decoration: BoxDecoration(
+        border: Border(
+          top: BorderSide(
+            color: purpleBox,
+            width: 0,
+          ),
+        ),
+        color: ultraVioletBox,
+      ),
+      child: Material(
+        color: purpleBox,
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(60),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            vertical: 24.0,
+            horizontal: 32.0,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              const SizedBox(
+                height: 4,
+              ),
+              const Text(
+                'TUESDAY 5:30 P.M.',
+                style: TextStyle(
+                  fontWeight: FontWeight.w300,
+                  color: Colors.white,
+                ),
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              const Text(
+                'Practice French, English and Chinese',
+                style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  fontSize: 24,
+                  color: Colors.white,
+                ),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      width: 50,
+                      child: Stack(
+                        children: <Widget>[
+                          Positioned(
+                            child: avatarBuilder('3.jpg'),
+                            left: 15,
+                          ),
+                          avatarBuilder('5.jpg'),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 16,
+                    ),
+                    const Text(
+                      'Bruce Wayne, Sam and 17 others',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w300,
+                        fontStyle: FontStyle.italic,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class UltraVioletBox extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 190,
+      decoration: BoxDecoration(
+        border: Border(
+          top: BorderSide(
+            color: ultraVioletBox,
+            width: 0,
+          ),
+        ),
+        color: greyBox,
+      ),
+      child: Material(
+        color: ultraVioletBox,
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(60),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            vertical: 24.0,
+            horizontal: 32.0,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              const SizedBox(
+                height: 4,
+              ),
+              const Text(
+                'FRIDAY 6:00 P.M.',
+                style: TextStyle(
+                  fontWeight: FontWeight.w300,
+                  color: Colors.white,
+                ),
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              const Text(
+                'Adobe XD Live Event, somewhere... I guess.',
+                style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  fontSize: 24,
+                  color: Colors.white,
+                ),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      width: 50,
+                      child: Stack(
+                        children: <Widget>[
+                          Positioned(
+                            child: avatarBuilder('6.jpg'),
+                            left: 15,
+                          ),
+                          avatarBuilder('7.jpg'),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 16,
+                    ),
+                    const Text(
+                      'Samuel Jackson, Cranky and 89 others',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w300,
+                        fontStyle: FontStyle.italic,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
